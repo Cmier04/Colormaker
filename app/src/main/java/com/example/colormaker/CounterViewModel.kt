8aef807385f23423e643e859e91e3f73a765a010
Cmier04 <112023982+Cmier04@users.noreply.github.com>
2025-04-22 20:16:57 -0700

    test

---
+++ app/src/main/java/com/example/colormaker/CounterViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.colormaker
+
+class CounterViewModel {
+
+}
--- app/src/main/java/com/example/colormaker/MainActivity.kt
+++ app/src/main/java/com/example/colormaker/MainActivity.kt
@@ -1,32 +1,87 @@
 package com.example.colormaker
 
+//import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
+//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.lifecycle.ViewModelProvider
+
+/*import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Toast
-import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.SwitchCompat*/
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var decreaseButton: Button
+    private lateinit var increaseButton: Button
+    private lateinit var counterView: TextView
+
+    private var counter = -99
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        this.decreaseButton = this.findViewById(R.id.DecreaseButton)
+        increaseButton = findViewById(R.id.IncreaseButton)
+        counterView = findViewById(R.id.counter)
+
+        counter = counterView.text.toString().toInt()
+
+        //@SuppressLint("SetTextI18n")
+        fun updateCounterView() {
+            counterView.text = "$counter"
         }
-        val switch1: SwitchCompat = findViewById(R.id.switch1)
-        switch1.setOnClickListener { _, isChecked ->
-            val message = if (isChecked as Boolean) "Switch1: ON" else "Switch1: OFF"
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+        updateCounterView()
+
+        decreaseButton.setOnClickListener( { view: View ->
+            println("decrease button clicked")
+            counter -= 1
+            updateCounterView()
+        } )
+
+        increaseButton.setOnClickListener {
+            val counterVal = 
+            val textOfButton = button.text
+            println("$textOfButton button clicked")
+            counter -= 1
+            updateCounterView()
         }
     }
-}
-
-private fun SwitchCompat.setOnClickListener(function: (v: View, Any?) -> Unit) {
-    TODO("Not yet implemented")
+//
+//    private fun x() {
+//        var x1 = 1
+//        y()
+//        z(30)
+//        a()
+//    }
+//
+//    private fun a() {
+//        val a1 = 5
+//        val a2 = 10
+//        for (n in 0..a1) {
+//            val nn = n * 10
+//            println("$nn")
+//            for (m in 0..a2) {
+//                println("the value of m is ${m}")
+//            }
+//        }
+//    }
+//
+//    private fun z(p: Int): String {
+//        val z1 = 2
+//        println("I am in z and the value of my variable is ${z1 + p}")
+//        return "done"
+//    }
+//
+//    private fun y() {
+//        println("I am in y")
+//        val f = z(20)
+//        println(f)
+//    }
 }
